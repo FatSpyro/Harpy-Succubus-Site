abd13124c1bdd3d8936ac5e7467fb339333f1463
Emily Hempker <hempkeey@mail.uc.edu>
2023-01-24 00:33:19 -0500

    imagegallery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('I am connected'); 
+var imgBox = document.getElementById("imgBox"); 
+var fullImg = document.getElementById("imgDescriptionBox"); 
+
+function openFullImg(pic){
+    fullImg.style.display = "inline"; 
+    imgBox.src = pic; 
+}
+
+function closeFullImg(){
+    fullImg.style.display = "none"; 
+}
